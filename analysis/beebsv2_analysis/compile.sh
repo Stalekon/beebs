0c5bbdbeffa854c48d88125c9b32827dc9c200cc
Stoil Ganev <sg13286@my.bristol.ac.uk>
2014-08-07 12:25:59 +0100

    Forgot to remove old version

--- analysis/beebsv2_analysis/compile.sh
+++
@@ -1,5 +0,0 @@
-plugin=../../gcc-python-plugin/python.so
-script=../analysis/beebsv2_analysis/gimple-analyse.py
-make LD_LIBRARY_PATH=../../gcc-python-plugin/gcc-c-api CFLAGS="-fplugin=$plugin -fplugin-arg-python-script=$script"
-
-
